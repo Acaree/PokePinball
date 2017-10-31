cd5b794cff945a671b05b2d5106cd931af6fb4f3
Acaree <acampamar@gmail.com>
2017-10-31 11:30:56 +0100

    changing modules

--- ModulePhysics.h
+++ ModulePhysics.h
@@ -42,41 +42,23 @@ public:
 	update_status Update();
 	update_status PostUpdate();
 	bool CleanUp();
-	void CreateMapObstacles();
+	
 
 	PhysBody* CreateCircle(int x, int y, int radius, b2BodyType type);
 	PhysBody* CreateRectangle(int x, int y, int width, int height, b2BodyType type);
 	PhysBody* CreateRectangleSensor(int x, int y, int width, int height);
 	PhysBody* CreateChain(int x, int y, int* points, int size, b2BodyType type);
 
-	PhysBody* left_flipper_anchor;
-	PhysBody* left_flipper;
-	PhysBody* right_flipper_anchor;
-	PhysBody* right_flipper;
-	PhysBody* left_triangle;
-	PhysBody* right_triangle;
-	PhysBody* left_bounce;
-	PhysBody* right_bounce;
-	PhysBody* shelder1;
-	PhysBody* shelder2;
-	PhysBody* shelder3;
+	b2World* world;
+	
 	// b2ContactListener ---;
 	void BeginContact(b2Contact* contact);
 
 private:
 
-	PhysBody *bouncy, *pivotBouncy;
-	b2RevoluteJoint* joint_left_flipper;
-	b2RevoluteJoint* joint_right_flipper;
-
-	b2BodyDef dook;
-	SDL_Texture* map;
-	SDL_Texture* left_flipper_png;
-	SDL_Texture* right_flipper_png;
-	SDL_Texture* shelder_png;
+	
 	b2Body* body_clicked = nullptr;
 	bool debug;
-	b2World* world;
 	b2MouseJoint* mouse_joint;
 	b2Body* ground;
 };
--- ModuleSceneIntro.h
+++ ModuleSceneIntro.h
@@ -3,10 +3,11 @@
 #include "p2List.h"
 #include "p2Point.h"
 #include "Globals.h"
+#include "Box2D/Box2D/Box2D.h"
 
 class PhysBody;
 
-class ModuleSceneIntro : public Module
+class ModuleSceneIntro : public Module, public b2ContactListener
 {
 public:
 	ModuleSceneIntro(Application* app, bool start_enabled = true);
@@ -16,11 +17,21 @@ public:
 	update_status Update();
 	bool CleanUp();
 	void OnCollision(PhysBody* bodyA, PhysBody* bodyB);
+	void CreateMapObstacles();
 
 public:
 	PhysBody* circles;
-	p2List<PhysBody*> boxes;
-	p2List<PhysBody*> ricks;
+	PhysBody* left_flipper_anchor;
+	PhysBody* left_flipper;
+	PhysBody* right_flipper_anchor;
+	PhysBody* right_flipper;
+	PhysBody* left_triangle;
+	PhysBody* right_triangle;
+	PhysBody* left_bounce;
+	PhysBody* right_bounce;
+	PhysBody* shelder1;
+	PhysBody* shelder2;
+	PhysBody* shelder3;
 
 	PhysBody* sensor;
 	bool sensed;
@@ -32,4 +43,16 @@ public:
 	uint bonus_fx;
 	p2Point<int> ray;
 	bool ray_on;
+
+	private:
+		PhysBody *bouncy;
+		PhysBody *pivotBouncy;
+		b2RevoluteJoint* joint_left_flipper;
+		b2RevoluteJoint* joint_right_flipper;
+
+		b2BodyDef dook;
+		SDL_Texture* map;
+		SDL_Texture* left_flipper_png;
+		SDL_Texture* right_flipper_png;
+		SDL_Texture* shelder_png;
 };
